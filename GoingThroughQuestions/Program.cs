4bf5a543c835981315ce67fc99660f2f0a1b4753
Pricopi Bianca <pb31627@gmail.com>
2022-05-08 14:29:28 +0300

    encapsulation

---
+++ GoingThroughQuestions/Program.cs
@@ -0,0 +1,44 @@
+﻿public class User
+{
+    string name;
+    private string address;
+
+    public string Address
+    {
+        get
+        {
+            return address;
+        }
+        set
+        {
+            address = value;
+        }
+    }
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            name = value;
+        }
+    }
+}
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        User u = new User();
+
+        u.Name = "Denis";
+        u.Address = "Germany";
+
+        Console.WriteLine("Name: " + u.Name);
+        Console.WriteLine("Address: " + u.Address);
+        Console.WriteLine("Press Enter Key...");
+        Console.ReadLine();
+    }
+}
