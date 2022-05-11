031f5c9b8b8c488a87266aa8d2150929a46fd244
Pricopi Bianca <pb31627@gmail.com>
2022-05-11 12:42:20 +0300

    found a better solution on leetcode

--- RomanToInt/Program.cs
+++ RomanToInt/Program.cs
@@ -98,11 +98,39 @@
     }
 }
 
+public class BetterSolution
+{
+    public static void Solution(string num)
+    {
+        Dictionary<char, int> romanNums = new() { { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, { 'L', 50 }, { 'C', 100 }, { 'D', 500 }, { 'M', 1000 } };
+
+        int result = 0;
+        for (int i = num.Length - 1; i >= 0; i--)
+        {
+            if(i == 0)
+            {
+                result += romanNums[num[i]];
+            }
+            else if(romanNums[num[i]] > romanNums[num[i - 1]])
+            {
+                result += romanNums[num[i]] - romanNums[num[i - 1]];
+                i--;
+            }
+            else
+            {
+                result += romanNums[num[i]];
+            }
+        }
+        Console.WriteLine("Better solution: " + result);
+    }
+}
+
 public class Test
 {
     public static void Main()
     {
         Solution solution = new Solution();
-        Console.WriteLine(solution.RomanToInt("IX"));
+        Console.WriteLine("My solution: " + solution.RomanToInt("MCMXCIV"));
+        BetterSolution.Solution("MCMXCIV");
     }
 }
