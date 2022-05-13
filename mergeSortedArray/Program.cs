1381eeb894591ba551ebc23a19e0f8aca1dc7d96
Pricopi Bianca <pb31627@gmail.com>
2022-05-13 22:52:41 +0300

    30/59 tests

--- mergeSortedArray/Program.cs
+++ mergeSortedArray/Program.cs
@@ -3,15 +3,12 @@
     public void Merge(int[] nums1, int m, int[] nums2, int n)
     {
         int j = 0;
+        int added = m;
         for (int i = 0; i < nums1.Length; i++)
         {
             if (j < n)
             {
-                if (i > m || m == 0)
-                {
-                    nums1[i] = nums2[j++];
-                }
-                else if (nums2[j] <= nums1[i])
+                if (nums2[j] <= nums1[i])
                 {
                     int k;
                     for (k = nums1.Length - 1; k > i; k--)
@@ -20,12 +17,19 @@
 
                     }
                     nums1[k] = nums2[j++];
+                    i++;
                 }
+                if (m <= 0)
+                {
+                    nums1[i] = nums2[j++];
+                }
+                m--;
 
             }
+            
         }
 
-for (int i = 0; i < m + n; i++)
+for (int i = 0; i < nums1.Length; i++)
         {
             Console.Write(nums1[i] + " ");
         }
@@ -37,8 +41,8 @@ public class Test
     public static void Main()
     {
         Solution solution = new Solution();
-        int[] nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
-        int[] nums2 = new int[] { 2, 5, 6 };
-        solution.Merge(nums1, 3, nums2, nums2.Length);
+        int[] nums1 = new int[] { 1, 0};
+        int[] nums2 = new int[] { 2 };
+        solution.Merge(nums1, 1, nums2, nums2.Length);
     }
 }
