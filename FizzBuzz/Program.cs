5d17c7d8851e662c71f9420f2e249670c818a1c5
Pricopi Bianca <pb31627@gmail.com>
2022-05-13 22:26:15 +0300

    not working

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Solution
+{
+    public IList<string> FizzBuzz(int n)
+    {
+        IList<string> fizzBuzz = new List<string>();
+        for (int i = 1; i <= n; i++)
+        {
+            if(i % 3 == 0)
+            {
+                if(i % 5 == 0)
+                {
+                    fizzBuzz.Add("FizzBuzz");
+                }
+                else
+                {
+                    fizzBuzz.Add("Fizz");
+                }
+            }
+            else if(i % 5 == 0)
+            {
+                fizzBuzz.Add("Buzz");
+            }
+            else
+            {
+                fizzBuzz.Add(i.ToString());
+            }
+        }
+        return fizzBuzz;
+    }
+}
+
+public class Test
+{
+    public static void Main()
+    {
+        Solution solution = new Solution();
+
+        IList<string> fizzBuzz = solution.FizzBuzz(15);
+        foreach(string f in fizzBuzz)
+        {
+            Console.Write(f + " ");
+        }
+    }
+}
---
+++ mergeSortedArray/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int j = 0;
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            if (j < n)
+            {
+                if (i > m || m == 0)
+                {
+                    nums1[i] = nums2[j++];
+                }
+                else if (nums2[j] <= nums1[i])
+                {
+                    int k;
+                    for (k = nums1.Length - 1; k > i; k--)
+                    {
+                        nums1[k] = nums1[k - 1];
+
+                    }
+                    nums1[k] = nums2[j++];
+                }
+
+            }
+        }
+
+for (int i = 0; i < m + n; i++)
+        {
+            Console.Write(nums1[i] + " ");
+        }
+    }
+}
+
+public class Test
+{
+    public static void Main()
+    {
+        Solution solution = new Solution();
+        int[] nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+        int[] nums2 = new int[] { 2, 5, 6 };
+        solution.Merge(nums1, 3, nums2, nums2.Length);
+    }
+}
